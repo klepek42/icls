8b7c7ce64d142425bf76b9abe8d59878c4bb5144
Edgar Klepek <edgarklepek@gmail.com>
2016-02-13 01:08:32 +0100

    Base project

---
+++ app/src/test/java/fhdw/mfwx413/flyingdutchmen/icls/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package fhdw.mfwx413.flyingdutchmen.icls;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
