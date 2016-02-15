1cf812169a590f899e06454ae8efadba06b7c239
edgar_klepek <edgarklepek@gmail.com>
2016-02-15 14:22:24 +0100

    Testing the layout of ChallengeFreeAnswer as the starting layout

--- app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/activities/StartMenu/Init.java
+++ app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/activities/StartMenu/Init.java
@@ -3,10 +3,13 @@ package fhdw.mfwx413.flyingdutchmen.icls.activities.StartMenu;
 import android.app.Activity;
 import android.os.Bundle;
 
+import fhdw.mfwx413.flyingdutchmen.icls.R;
+
 public class Init extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_free_answer);
     }
 }
