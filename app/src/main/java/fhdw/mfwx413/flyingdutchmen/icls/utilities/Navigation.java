85171a56ef7bbf055151b554e706d91f427e6b63
Edgar Klepek <edgarklepek@gmail.com>
2016-02-20 19:47:46 +0100

    Created startActivity functions for all activites and demo csv files

--- app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/utilities/Navigation.java
+++ app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/utilities/Navigation.java
@@ -13,8 +13,84 @@ public class Navigation {
 
     public static void startActivityAddNewUser(Activity callingActivity) {
         Intent intent;
-
         intent = new Intent(callingActivity, fhdw.mfwx413.flyingdutchmen.icls.activities.AddNewUser.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityChallengeFreeAnswer(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.ChallengeFreeAnswer.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityChallengeImagineAnswer(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.ChallengeImagineAnswer.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityChallengeMultipleChoice(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.ChallengeMultipleChoice.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityChooseFile(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.ChooseFile.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityEditUser(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.EditUser.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityFeedbackChallengeRest(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.FeedbackChallengeRest.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityFeedbackImagineAnswer(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.FeedbackImagineAnswer.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityFinalEndOfChallenges(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.FinalEndOfChallenges.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivitySettingMenu(Activity callingActivity){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.SettingsMenu.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityStartMenu(Activity callingActivity, int currentChallengeId, int numberCorrectChallenges){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.StartMenu.Init.class);
+        callingActivity.startActivity(intent);
+        callingActivity.finish();
+    }
+
+    public static void startActivityStatistics(Activity callingActivity, int currentChallengeId, int numberCorrectChallenges){
+        Intent intent;
+        intent = new Intent(callingActivity,fhdw.mfwx413.flyingdutchmen.icls.activities.Statistics.Init.class);
         callingActivity.startActivity(intent);
         callingActivity.finish();
     }
