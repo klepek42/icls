cb17bbdae63a8ac8eec477131194580f0766e996
carllehaze <pascalhess@web.de>
2016-03-05 00:05:13 +0100

    Kommentare in beiden Feedback-Activities angepasst jeweils nur in den beiden ApplicationLogic(s)

--- app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/activities/FeedbackChallengeRest/ApplicationLogic.java
+++ app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/activities/FeedbackChallengeRest/ApplicationLogic.java
@@ -4,15 +4,14 @@ import android.app.Activity;
 import android.util.Log;
 import android.widget.Toast;
 
-import fhdw.mfwx413.flyingdutchmen.icls.data.UserProgressCollection;
 import fhdw.mfwx413.flyingdutchmen.icls.exceptions.InvalidCorrectAnswerTypeException;
 import fhdw.mfwx413.flyingdutchmen.icls.exceptions.InvalidQuestionTypeLayoutException;
 import fhdw.mfwx413.flyingdutchmen.icls.utilities.Navigation;
 
-
 /**
  * Responsibility: Pascal Heß
  */
+
 public class ApplicationLogic {
 
     private Data mData;
@@ -27,112 +26,124 @@ public class ApplicationLogic {
         initialUpdateGui();
     }
 
+
     //initialize GUI by setting the Feedbacktexts
     private void initialUpdateGui() {
         String correctAnswer;
 
-        //delivers String with the correct answer(s)
         try {
+            //delivers String with the correct answer(s)
             correctAnswer = mData.getmDueChallengesOfUserInFile().getChallenge(mData.getmCurrentChallengeId()).getmCorrectAnswerString();
 
-            //set feedback field with the real correct answer
+            //set the real correct answer(s) into the feedback field
             mGui.setFeedbackTextCorrectAnswer(correctAnswer);
 
-            //if the answer was correct
+            //check if the answer was correct
             if (mData.getmIsAnswerCorrect() == true) {
+                //if yes, set the feedback-fields with the following
                 mGui.setFeedbackText("Die Antwort war korrekt");
                 mGui.setFeedbackTextUpDownGrade("Die Frage wird heraufgestuft");
             } else {
-                //if the answer was false
+                //if not, set the feedback-fields with the following:
                 mGui.setFeedbackText("Die Antwort war falsch");
                 mGui.setFeedbackTextUpDownGrade("Die Frage wird wieder heruntergestuft");
             }
         }
+        //Exception-Handling, if there is a invalid correct answer type
         catch (InvalidCorrectAnswerTypeException e){
             Log.e("ICLS-ERROR", "FeedbackChallengeRest::ApplicationLogic::initialUpdateGui ", e);
             Toast.makeText(mActivity, "Unerwarteter Fehler", Toast.LENGTH_SHORT).show();
             Navigation.startActivityChooseFile(mData.getActivity(), mData.getmChosenUser());
         }
-
     }
 
-    //method that is invoked if the continue button is clicked
-    //call the next due activity and send the required data
+
+    //method that is invoked, if the continue button is clicked
     public void onButtonContinue() throws InvalidQuestionTypeLayoutException {
         int dueChallengeNumber;
 
             //delivers int value, to decide which Activity has to be started
             dueChallengeNumber = otherDueChallenges();
 
-            //it depends on the layoutType of the next challenge, which activity is to be started next
+            //it depends on the layout-type of the next due challenge, which activity is to be started next
+            //Switch-Statement determines which layout has to be started and which data have to be sent
             switch (dueChallengeNumber) {
                 //no other challenge is due
                 case 0:
                     Navigation.startActivityFinalEndOfChallenges(mData.getActivity(), mData.getmChosenUser(), mData.getmChosenFile());
                     break;
-                case 1: //a challenge of type ChallengeFreeAnswer is due
-                    //set the next challenge ID by adding 1; this is required to start the correct "next" activity
+                //a challenge of type ChallengeFreeAnswer is due
+                case 1:
+                    //the old challenge ID is incremented by 1, so the correct "next" activity can be started and the correct information can be sent
                     mData.incrementChallengeIdByOne();
                     Navigation.startActivityChallengeFreeAnswer(mData.getActivity(), mData.getmDueChallengesOfUserInFile(), mData.getmCurrentChallengeId(), mData.getmChosenUser(), mData.getmChosenFile(), mData.getmCurrentUserProgresses());
                     break;
-                case 2: //a challenge of type ChallengeImagineAnser is due
-                    //set the next challenge ID by adding 1; this is required to start the correct "next" activity
+                //a challenge of type ChallengeImagineAnser is due
+                case 2:
+                    //the old challenge ID is incremented by 1, so the correct "next" activity can be started and the correct information can be sent
                     mData.incrementChallengeIdByOne();
                     Navigation.startActivityChallengeImagineAnswer(mData.getActivity(), mData.getmDueChallengesOfUserInFile(), mData.getmCurrentChallengeId(), mData.getmChosenUser(), mData.getmChosenFile(), mData.getmCurrentUserProgresses());
                     break;
-                case 3: //a challenge of type ChallengeMultipleChoice is due
-                    //set the next challenge ID by adding 1; this is required to start the correct "next" activity
+                //a challenge of type ChallengeMultipleChoice is due
+                case 3:
+                    //the old challenge ID is incremented by 1, so the correct "next" activity can be started and the correct information can be sent
                     mData.incrementChallengeIdByOne();
                     Navigation.startActivityChallengeMultipleChoice(mData.getActivity(), mData.getmDueChallengesOfUserInFile(), mData.getmCurrentChallengeId(), mData.getmChosenUser(), mData.getmChosenFile(), mData.getmCurrentUserProgresses());
                     break;
+                //Exception-Handling, if there is a int-value in "dueChallengeNumber" that is not 0,1,2 or 3
                 default:
                     throw new InvalidQuestionTypeLayoutException("FeedbackChallengeRest::ApplicationLogic::onButtonContinue");
             }
-
         }
+
 
     //method that is invoked, if the abort button is clicked
     public void onButtonAbortClicked() {
         Navigation.startActivityChooseFile(mData.getActivity(), mData.getmChosenUser());
     }
 
+
     //method that is invoked, if the standard back button (on the phone or tablet) is clicked
     public void onStandardBackButtonClicked() {
         Navigation.startActivityChooseFile(mData.getActivity(), mData.getmChosenUser());
     }
 
-    //Methode delivers int value, whether there are other Challenges due or not
-    //value: 0 --> no other challenge is due, Type FinalEndOfChallenges can be started
-    //value: 1 --> another challenge is due, Type ChallengeFreeAnswer can be started
-    //value: 2 --> another challenge is due, Type ChallengeImagineAnswer can be started
-    //value: 3 --> another challenge is due, Type ChallengeMultipleChoice can be started
-    //value: 4 --> Error
+
+    //method returns an int-value, that gives information whether there is another due challenge and of which type it is
     public int otherDueChallenges() {
         int dueChallengeNumber = -1;
         int numberOfDueChallengesOfUserInFile;
         int currentChallengeId;
         int nextChallengeId;
 
+        //get the total number of due challenges in the current file of the user
         numberOfDueChallengesOfUserInFile = mData.getmDueChallengesOfUserInFile().getSize();
+        //get the challenge ID of the current challenge
         currentChallengeId = mData.getmCurrentChallengeId();
 
-        //check if there are further challenges to be started
+        //check if there are further challenges to be started (check if there is a next due challenge after the present one
         if (numberOfDueChallengesOfUserInFile > (currentChallengeId + 1)) {
+            //if there are other due challenges do the following:
 
-            //compute the type of the next challenge for the switch-statement
+            //set the next challenge ID
             nextChallengeId = currentChallengeId + 1;
+
+            //compute the layout-type of the next challenge and safe it as an int-value
+                // "1": layout type ChallengeFreeAnswer
+                // "2": layout type ChallengeImagineAnswer
+                // "3": layout type ChallengeMultipleChoice
             dueChallengeNumber = mData.getmDueChallengesOfUserInFile().getChallenge(nextChallengeId).getmQuestionTypeLayout();
         }
         else {
+            //if there are no other du challenges do the following:
             dueChallengeNumber = 0;
         }
-
         return dueChallengeNumber;
     }
 
-    //Error-Handling for the method which computes the nex activity-layout
+    //Error-Handling for the false next activity-layout
+    //it starts, when the int-value of the layout type is not 0,1,2 or 3
     public void errorToastFalseLayout() {
-        Toast.makeText(mActivity, "Unerwartetes layout", Toast.LENGTH_SHORT).show();
+        Toast.makeText(mActivity, "Unerwartetes Layout", Toast.LENGTH_SHORT).show();
     }
 }
-
--- app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/activities/FeedbackImagineAnswer/ApplicationLogic.java
+++ app/src/main/java/fhdw/mfwx413/flyingdutchmen/icls/activities/FeedbackImagineAnswer/ApplicationLogic.java
@@ -6,12 +6,14 @@ import android.widget.Toast;
 
 import fhdw.mfwx413.flyingdutchmen.icls.data.UserProgressDatabase;
 import fhdw.mfwx413.flyingdutchmen.icls.exceptions.InvalidCorrectAnswerTypeException;
+import fhdw.mfwx413.flyingdutchmen.icls.exceptions.InvalidQuestionTypeLayoutException;
 import fhdw.mfwx413.flyingdutchmen.icls.exceptions.UserProgressNotFoundException;
 import fhdw.mfwx413.flyingdutchmen.icls.utilities.Navigation;
 
 /**
  * Responsibility: Pascal Heß
  */
+
 public class ApplicationLogic {
 
     private Data mData;
@@ -26,20 +28,22 @@ public class ApplicationLogic {
         initialUpdateGui();
     }
 
+
     //initialize GUI by setting the Feedbacktexts
     private void initialUpdateGui() {
         String correctAnswer;
 
-        //set the text "Die korrekten Antworten sind" in the textview
+        //set the string "Die korrekten Antworten sind" into the textview
         mGui.setTextViewWasAnswerCorrect();
 
-        //delivers String with the correct answer(s)
         try {
+            //delivers String with the correct answer(s)
             correctAnswer = mData.getmDueChallengesOfUserInFile().getChallenge(mData.getmCurrentChallengeId()).getmCorrectAnswerString();
 
-            //set feedback field with the real correct answer
+            //set the real correct answer(s) into the feedback field
             mGui.setTextViewCorrectAnswer(correctAnswer);
         }
+        //Error-Handling if there is a invalid correct answer type
         catch (InvalidCorrectAnswerTypeException e){
             Log.e("ICLS-ERROR", "FeedbackImagineAnswer::ApplicationLogic::initialUpdateGui ", e);
             Toast.makeText(mActivity, "Unerwarteter Fehler", Toast.LENGTH_SHORT).show();
@@ -47,19 +51,22 @@ public class ApplicationLogic {
         }
     }
 
+
+    //method that is invoked, if the button "buttonWasAnswerCorrect" is clicked
     public void onButtonWasAnswerCorrect (){
         boolean isAnswerCorrect = true;
 
         try {
-            //User progress has to be updated (here it is a upgrade)
+            //the current user progress has to be updated (here it is a upgrade)
             updateUserProgress(isAnswerCorrect);
 
-            //set the next challenge ID by adding 1; this is required to start the correct "next" activity
+            //the old challenge ID is incremented by 1, so the correct "next" activity can be started and the correct information can be sent
             mData.incrementChallengeIdByOne();
 
-            //start the next activity
+            //checks whether there is a next activity that is to be started and starts the next activity with the correct layout type
             startNextActivity();
         }
+        //Exception-Handling, if there was a problem in finding the user progress
         catch (UserProgressNotFoundException e) {
             Log.e("ICLS-LOG", "ChallengeFreeAnswer::ApplicationLogic::onButtonConfirmFreeAnswerClicked: ", e);
             showErrorUnexpectedError();
@@ -67,19 +74,22 @@ public class ApplicationLogic {
         }
     }
 
+
+    //method that is invoked, if the button "buttonWasAnswerWring" is clicked
     public void onButtonWasAnswerWrong (){
         boolean isAnswerCorrect = false;
 
         try {
-            //User progress has to be updated (here it is a downgrade)
+            //the current user progress has to be updated (here it is a downgrade)
             updateUserProgress(isAnswerCorrect);
 
-            //set the next challenge ID by adding 1; this is required to start the correct "next" activity
+            //the old challenge ID is incremented by 1, so the correct "next" activity can be started and the correct information can be sent
             mData.incrementChallengeIdByOne();
 
-            //start the next activity
+            //checks whether there is a next activity that is to be started and starts the next activity with the correct layout type
             startNextActivity();
         }
+        //Exception-Handling, if there was a problem in finding the user progress
         catch (UserProgressNotFoundException e){
             Log.e("ICLS-LOG", "ChallengeFreeAnswer::ApplicationLogic::onButtonConfirmFreeAnswerClicked: ", e);
             showErrorUnexpectedError();
@@ -87,19 +97,21 @@ public class ApplicationLogic {
         }
     }
 
+    //method that computes the next due activity and the correct layout type that belongs to it, then this activitiy is started
     public void startNextActivity() {
         int dueChallengeNumber;
 
         //delivers int value, to decide which Activity has to be started
         dueChallengeNumber = otherDueChallenges();
 
-        //it depends on the layoutType of the next challenge, which activity is to be started next
+        //it depends on the layout-type of the next due challenge, which activity is to be started next
+        //Switch-Statement determines which layout has to be started and which data have to be sent
         switch (dueChallengeNumber) {
             //no other challenge is due
             case 0:
                 Navigation.startActivityFinalEndOfChallenges(mData.getActivity(), mData.getmChosenUser(), mData.getmChosenFile());
                 break;
-                //a challenge of type ChallengeFreeAnswer is due
+            //a challenge of type ChallengeFreeAnswer is due
             case 1:
                 Navigation.startActivityChallengeFreeAnswer(mData.getActivity(), mData.getmDueChallengesOfUserInFile(), mData.getmCurrentChallengeId(), mData.getmChosenUser(), mData.getmChosenFile(), mData.getmCurrentUserProgresses());
                 break;
@@ -111,64 +123,80 @@ public class ApplicationLogic {
             case 3:
                 Navigation.startActivityChallengeMultipleChoice(mData.getActivity(), mData.getmDueChallengesOfUserInFile(), mData.getmCurrentChallengeId(), mData.getmChosenUser(), mData.getmChosenFile(), mData.getmCurrentUserProgresses());
                 break;
-            //Error handling is to be implemented here
-            case 4:
-                Log.wtf("Fehler bei Errechnung des nächsten Challengetyps", "Error");
-                break;
+            //Exception-Handling, if there is a int-value in "dueChallengeNumber" that is not 0,1,2 or 3
+            default:
+                //Todo: Error-Handling has to be implemented - throw exception
+                //Todo: FeedbackChallengeRest outsource this function as well?!
         }
     }
 
-    //Methode delivers int value, whether there are other Challenges due or not
-    //value: 0 --> no other challenge is due, Type FinalEndOfChallenges can be started
-    //value: 1 --> another challenge is due, Type ChallengeFreeAnswer can be started
-    //value: 2 --> another challenge is due, Type ChallengeImagineAnswer can be started
-    //value: 3 --> another challenge is due, Type ChallengeMultipleChoice can be started
-    //value: 4 --> Error
+
+    //method returns an int-value, that gives information whether there is another due challenge and of which type it is
     private int otherDueChallenges() {
         int dueChallengeNumber = 4;
         int numberOfDueChallengesOfUserInFile;
         int currentChallengeId;
         int nextChallengeId;
 
+        //get the total number of due challenges in the current file of the user
         numberOfDueChallengesOfUserInFile = mData.getmDueChallengesOfUserInFile().getSize();
+        //get the challenge ID of the current challenge
         currentChallengeId = mData.getmCurrentChallengeId();
 
-        //check if there are further challenges to be started
+        //check if there are further challenges to be started (check if there is a next due challenge after the present one
         if (numberOfDueChallengesOfUserInFile > (currentChallengeId + 1)) {
+            //if there are other due challenges do the following:
 
-            //compute the type of the next challenge for the switch-statement
+            //set the next challenge ID
             nextChallengeId = currentChallengeId + 1;
+
+            //compute the layout-type of the next challenge and safe it as an int-value
+            // "1": layout type ChallengeFreeAnswer
+            // "2": layout type ChallengeImagineAnswer
+            // "3": layout type ChallengeMultipleChoice
             dueChallengeNumber = mData.getmDueChallengesOfUserInFile().getChallenge(nextChallengeId).getmQuestionTypeLayout();
         }
         else {
+            //if there are no other du challenges do the following:
             dueChallengeNumber = 0;
         }
 
         return dueChallengeNumber;
     }
 
-    //method, that saves the user progress (whether the question was answered correctly or not)
+
+    //method, that refreshes and saves the user progress
+    //that depends on whether the question was answered correctly or not
     private void updateUserProgress(boolean isAnswerCorrect) throws UserProgressNotFoundException{
         boolean userProgressFound = false;
+        //run with the for-loop through the collection "CurrentUserProgresses", to find the correct one
         for (int i = 0; i < mData.getmCurrentUserProgresses().getSize(); i++){
+            //check, if the challenge ID of the collection (of the current for-loop) equals the challenge ID of the current challenge
             if (mData.getmCurrentUserProgresses().getUserProgress(i).getmChallengeID() == mData.getmDueChallengesOfUserInFile().getChallenge(mData.getmCurrentChallengeId()).getmID()){
                 int actualTimeClass = mData.getmCurrentUserProgresses().getUserProgress(i).getmPeriodClass();
                 userProgressFound = true;
                 mData.getmCurrentUserProgresses().getUserProgress(i).setCurrentTimeStamp();
+                //check if the given answer was correct or not
                 if (isAnswerCorrect == true) {
+                    //if the answer was correct, do the following:
                     if (actualTimeClass < 5 ) {
+                        //as long as the current time-class is lower than "5", it can be incremented by 1
                         mData.getmCurrentUserProgresses().getUserProgress(i).setmPeriodClass(actualTimeClass + 1);
                     }
                 }
                 else {
+                    //if the answer was false do the following:
                     if (actualTimeClass > 1 ) {
+                        //as long as the current time-class is greater than "1", it can be decremented by 1
                         mData.getmCurrentUserProgresses().getUserProgress(i).setmPeriodClass(actualTimeClass - 1);
                     }
                 }
+                //Save the current user progress in the user progress database
                 UserProgressDatabase.writeSpecificUserProgresses(mData.getmCurrentUserProgresses(), mData.getmChosenUser().getName(), mActivity);
                 break;
             }
         }
+        //Exception-Handling, if there was a problem in finding the correct user progress, that belongs to this user
         if (userProgressFound == false){
             throw new UserProgressNotFoundException("ChallengeFreeAnswer::ApplicationLogic::updateUserProgress:"
                     + " CurrentUserName: " + mData.getmChosenUser().getName()
@@ -176,7 +204,11 @@ public class ApplicationLogic {
         }
     }
 
+
+    //method that opens a toast for a few seconds on the screen
     public void showErrorUnexpectedError(){
         Toast.makeText(mActivity, "Unerwarteter Fehler", Toast.LENGTH_SHORT).show();
     }
+
+    //Todo: Implement method "errorToastFalseLayout" as well?! (--> FeedbackChallengeRest:ApplicationLogic)
 }
